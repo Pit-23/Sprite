4b7119d8cf87bb06c8cf78f01587bea417cbc1c1
Pit-23 <pbryan.maukar@gmail.com>
2025-11-04 13:39:25 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import numpy as np
+import cv2
+import insightface
+from insightface.app import FaceAnalysis
+from insightface.model_zoo import get_model
+import torch
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+
+import torchvision.transforms.functional as F
+import sys
+import types
+
+import os
+
+assert os.path.exists("/content/inswapper_128.onnx"), "inswapper missing"
+assert os.path.exists("/content/GFPGANv1.4.pth"), "GFPGAN missing"
+
+# Patch: emulate the old module path expected by basicsr
+sys.modules['torchvision.transforms.functional_tensor'] = types.ModuleType('torchvision.transforms.functional_tensor')
+sys.modules['torchvision.transforms.functional_tensor'].rgb_to_grayscale = F.rgb_to_grayscale
+
+from gfpgan import GFPGANer
+
+# Load models
+swapper = get_model('/content/inswapper_128.onnx', download=False, providers=['CPUExecutionProvider'])
+app = FaceAnalysis(name="buffalo_l", providers=['CPUExecutionProvider'])
+app.prepare(ctx_id=0, det_size=(640,640))
+restorer = GFPGANer(model_path='/content/GFPGANv1.4.pth', upscale=2, arch='clean', channel_multiplier=2)
+
+st.title("Sprite Face Swapper")
+
+name = st.text_input("Nama")
+place = st.text_input("Nama Warung")
+place = place.upper()
+address = st.text_input("Alamat")
+options = ["Jakarta","Surabaya"]
+city = st.selectbox("Kota", options)
+if city == "Jakarta" : 
+  option_daerah = ["Jakarta Utara", "Jakarta Selatan"]
+  daerah = st.selectbox("Kecamatan", option_daerah)
+  if daerah  == "Jakarta Utara" : 
+    option_kecamatan = ["PENJARINGAN", "PADEMANGAN", "TANJUNG PRIOK", "KOJA", "CILINCING", "KELAPA GADING"]
+    kecamatan = st.selectbox("Kecamatan", option_kecamatan)
+  else : 
+    option_kecamatan = ["Cilandak", "Jagakarsa", "Kebayoran Baru", "Kebayoran Lama", 
+                        "Mampang Prapatan", "Pancoran", "Pasar Minggu", "Pesanggrahan", "Setiabudi", "Tebet"]
+    kecamatan = st.selectbox("Kecamatan", option_kecamatan)
+
+elif city == "Surabaya" :
+  option_daerah = ["Surabaya Pusat", "Surabaya Selatan"]
+  daerah = st.selectbox("Daerah", option_daerah)
+  if daerah  == "Surabaya Pusat" : 
+    option_kecamatan = ["Genteng", "Tegalsari", "Bubutan", "Simokerto"]
+    kecamatan = st.selectbox("Kecamatan", option_kecamatan)
+  else :
+    option_kecamatan = ["Sawahan", "Wonokromo", "Dukuh Pakis", "Karangpilang", 
+                        "Wiyung", "Wonocolo", "Gayungan", "Jambangan"]
+    kecamatan = st.selectbox("Kecamatan", option_kecamatan)
+
+src_file = st.file_uploader("Upload Foto Selfie", type=['jpg', 'jpeg', 'png'])
+
+if src_file:
+    src_bytes = np.frombuffer(src_file.read(), np.uint8)
+
+    src_img = cv2.imdecode(src_bytes, cv2.IMREAD_COLOR)
+
+    st.image(cv2.cvtColor(src_img, cv2.COLOR_BGR2RGB),
+          caption=["Source"], width=300)
+    faces = app.get(src_img)
+    gen = faces[0].gender
+
+    if gen == 1:
+        st.text("Male face detected")
+        tgt_img = cv2.imread('Foto_Cowok (2).jpg', cv2.IMREAD_COLOR)
+    else:
+        st.text("Female face detected")
+        tgt_img = cv2.imread('Foto_Cewek (1).jpg', cv2.IMREAD_COLOR)
+
+
+    if st.button("Go"):
+      faces_target = app.get(tgt_img)
+      faces_source = app.get(src_img)
+
+      source_face = faces_source[0]
+      target_face = faces_target[0]
+
+      swapped = swapper.get(tgt_img, target_face, source_face, paste_back=True)
+      _, _, restored_img = restorer.enhance(swapped, has_aligned=False, only_center_face=False, paste_back=True)
+
+      
+      base = cv2.imread("Sprite.png")
+
+      overlay = cv2.resize(restored_img, (1000, 400))
+
+      y, x = 115,50
+
+      base[y:y+overlay.shape[0], x:x+overlay.shape[1]] = overlay
+
+      img_pil = Image.fromarray(base)
+
+      draw = ImageDraw.Draw(img_pil)
+
+      font = ImageFont.truetype("AktivGrotesk-Bold.ttf", 76)   # adjust 90 px font size
+
+      draw.text((1125, 225), place, font=font, fill="#39B75A")
+
+      # convert back to cv2 if you want to continue process
+      base = cv2.cvtColor(np.array(img_pil), cv2.COLOR_RGB2BGR)
+
+      Image.MAX_IMAGE_PIXELS = 500000000
+
+      st.image(cv2.cvtColor(restored_img, cv2.COLOR_RGB2BGR), caption="Final Enhanced Result", use_container_width=True)
+      st.image(base, caption="Final Result", use_container_width=True)
